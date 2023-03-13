e138eb0f1d4ac1b505781beae105db120868fda2
inpink <inpink@kakao.com>
2023-03-13 22:30:09 +0900

    Refactor : 필요없는 기능 리팩토링

--- inpink/_1주차/_2JadenCase문자열만들기/Solution.java
+++ inpink/_1주차/_2JadenCase문자열만들기/Solution.java
@@ -3,8 +3,6 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         
-        StringTokenizer st= new StringTokenizer(s);
-        
         String[] l= s.split("");
   
         StringBuilder sb= new StringBuilder();
@@ -18,24 +16,21 @@ class Solution {
                 if (before.equals(" ")){ //단어의 첫글자면
                     if (Character.isDigit(l[i].charAt(0))) { //첫글자가 정수면 
                         sb.append(l[i]);
-                        next=true;  
+                        next=true;
+                        
                     }
                     else { //첫글자가 문자면
                         sb.append(l[i].toUpperCase());
                     }
                 }
                 else { //단어의 두번째 글자부터
-                    if (next==true) { //숫자 다음글자라 소문자로 해줘야함
-                        sb.append(l[i].toLowerCase());
-                        next=false;
-                    }
-                    else{ //특별한것 없어서 다 소문자로 바꿔주면 됨 
-                        sb.append(l[i].toLowerCase());
-                    }
+                    sb.append(l[i].toLowerCase());
                 }
             }
             before = l[i];
     }
-    return sb.toString();     
+            return sb.toString();
+    
+        
     }
 }
