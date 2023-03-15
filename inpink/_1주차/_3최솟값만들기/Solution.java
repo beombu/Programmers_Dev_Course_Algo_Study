678671887ad7f09e14b2169814d83cfd6f502b1c
inpink <inpink@kakao.com>
2023-03-15 14:19:58 +0900

    Feat(Programmers) : 최솟값 만들기

---
+++ inpink/_1주차/_3최솟값만들기/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        int len=A.length;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for (int i=0; i<len; i++) answer+=(A[i]*B[len-i-1]);
+        
+        return answer;
+    }
+}
