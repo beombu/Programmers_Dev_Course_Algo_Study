542895a1eb4af942ea99560607d4db006a0cd58e
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-21 15:26:06 +0900

    Feat(Programmers): 피보나치 수

---
+++ bondyuu/피보나치 수/Solution.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(int n) {
+
+        int[] fib = new int[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i = 2; i <= n; i++){
+            fib[i] = fib[i-1] % 1234567 + fib[i-2] % 1234567;
+        }
+
+        return fib[n] %1234567;
+    }
+
+}
