672554ae1a54df173a4c5a6dc531f6b7c71ad676
beombu <sunil1369@bible.ac.kr>
2023-03-29 19:46:53 +0900

    Refactor(Programmers) : N개의 최소공배수

--- beombu/3주차/N개의_최소공배수.java
+++ beombu/3주차/N개의_최소공배수.java
@@ -1,25 +1,46 @@
-import java.util.HashMap;
-
+/**
+ * Time : O(n^2)
+ * 유클리드 호제법 : 최대공약수를 구하는 알고리즘(Greatest Common Divisor)
+ * (큰 수, 작은 수)로 나눈 나머지를 구하고 (나머지, 작은 수)를 나눈 나머지를 구하기를
+ * 반복하여 나머지가 0이 나올 때까지 반복하면 그때 의 작은 수가 최대공약수가 된다.
+ * 최소공배수 : 두 수의 곱 / 최대 공약수(Largest Common Multiple)
+ */
 public class N개의_최소공배수 {
     public static int solution(int[] arr) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int answer = 1;
+        int answer = arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j <= arr[i]; j++) {
-                if (arr[i] % j == 0) {
-                    map.put(j, map.getOrDefault(j, 0) + 1);
-                }
-            }
-            answer *= arr[i];
-        }
-
-        for (Integer key : map.keySet()) {
-            System.out.println("key : " + key + " value : " + map.get(key));
-            answer /= key;
+        for (int i = 1; i < arr.length; i++) {
+            answer = LCM(answer, arr[i]);
         }
 
         return answer;
+    }
+
+    //최소공배수 = a * b / 최대 공약수
+    public static int LCM(int a, int b) {
+        return a * b / GCD(a, b);
+    }
+
+    //최대공약수 구하는 공식
+    public static int GCD(int a, int b) {
+        //큰 수 비교
+        if (a < b) {
+            swap(a, b);
+        }
+
+        //나머지가 0이라면
+        if (b == 0) {
+            return a;
+        } else {
+            //다시 나누기
+            return GCD(b, a % b);
+        }
+    }
+
+    private static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
     }
 
     public static void main(String[] args) {
