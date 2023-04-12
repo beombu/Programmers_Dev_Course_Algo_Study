f90548539cfb5bba8f75719ac28d2b52e4f5b66e
beombu <sunil1369@bible.ac.kr>
2023-04-12 16:57:36 +0900

    Refactor(Programmers) : n^2 배열 자르기

--- beombu/5주차/n제곱_배열_자르기.java
+++ beombu/5주차/n제곱_배열_자르기.java
@@ -1,27 +1,17 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
- * O(n^2) 풀이로 런타임 에러와 메모리 초과 둘다 발생.
+ * Failure Commit : O(n^2) 풀이로 런타임 에러와 메모리 초과 둘다 발생.
+ * Time : O(n)
  */
 public class n제곱_배열_자르기 {
     public int[] solution(int n, long left, long right) {
-        int[] answer = new int[n * n];
-        //List<Integer> list = new ArrayList<>();
-        int idx = 0;
+        int[] answer = new int[(int) (right - left) + 1];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                //list.add(Math.max(i, j) + 1);
-                answer[idx++] = Math.max(i, j) + 1;
-            }
+        for (int i = 0; i < answer.length; i++) {
+            int row = (int) ((i + left) / n) + 1;
+            int col = (int) ((i + left) % n) + 1;
+            answer[i] = Math.max(row, col);
         }
 
-//      list.subList((int) (left - 1), (int) right);
-//      return list.stream().mapToInt(i -> i).toArray();
-
-        answer = Arrays.copyOfRange(answer, (int) left, (int) (right + 1));
         return answer;
     }
 }
