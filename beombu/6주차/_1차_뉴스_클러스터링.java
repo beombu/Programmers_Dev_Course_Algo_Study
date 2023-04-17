6b389a887bf71a92120fd71c6be4cbeb521a6c53
beombu <beombu13@gmail.com>
2023-04-17 17:35:13 +0900

    Refactor(Programmers) : 1차 뉴스 클러스터링

--- beombu/6주차/_1차_뉴스_클러스터링.java
+++ beombu/6주차/_1차_뉴스_클러스터링.java
@@ -1,35 +1,9 @@
 import java.util.HashMap;
-import java.util.HashSet;
 
-/**
- * 틀린 이유 : set을 사용하였기 때문에 두글자에 대한 갯수를 체크하지 못해 중복된 문자에서 문제 밠생
- */
-
-/**
- * 1. str1 loop(2문자씩)
- *      1-1. 문자인 경우
- *       - 소문자 변환
- *       - 저장소 저장
- *       - 합집합++
- *      1-2. 문자가 아닌 경우
- *       - continue
- *
- * 2. str2 loop(2문자씩)
- *      2-1. 문자인 경우
- *      - 소문자 변환
- *          2-1-1. 저장소에 값이 존재하는 경우
- *          - 교집합++
- *          2-1-2. 저장소에 값이 존재하지 않는 경우
- *          - 저장소 저장
- *          - 합집합++
- *      2-2. 문자가 아닌 경우
- *      - continue
- */
 public class _1차_뉴스_클러스터링 {
     public static int solution(String str1, String str2) {
-        int intersection = 0;//교집합
-        int combination = 0;//합집합
-        HashSet<String> set = new HashSet<>();
+        double intersection = 0;//교집합
+        double combination = 0;//합집합
         HashMap<String, Integer> map1 = new HashMap<>();
         HashMap<String, Integer> map2 = new HashMap<>();
 
@@ -38,12 +12,8 @@ public class _1차_뉴스_클러스터링 {
             char ch2 = str1.charAt(i + 1);
 
             if (isAlphabet(ch1, ch2)) {
-                String str = Character.toString(Character.toLowerCase(ch1))
-                        + Character.toString(Character.toLowerCase(ch2));
-                System.out.println("1 " + str);
-
-                set.add(str);
-                combination++;
+                String str = addChar(ch1, ch2);
+                map1.put(str, map1.getOrDefault(str, 0) + 1);
             }
         }
 
@@ -52,33 +22,35 @@ public class _1차_뉴스_클러스터링 {
             char ch2 = str2.charAt(i + 1);
 
             if (isAlphabet(ch1, ch2)) {
-                String str = Character.toString(Character.toLowerCase(ch1))
-                        + Character.toString(Character.toLowerCase(ch2));
-
-                System.out.println("2 " + str);
-
-                if (set.contains(str)) {
-                    intersection++;
-                } else {
-                    set.add(str);
-                    combination++;
-                }
+                String str = addChar(ch1, ch2);
+                map2.put(str, map2.getOrDefault(str, 0) + 1);
             }
         }
 
-        double answer = 0;
-        if (set.size() == 0) {
-            answer = 1;
-        } else if (set.size() == 1) {
-            answer = (double) Math.min(intersection, combination) / (double) Math.max(intersection, combination);
-        } else {
-            answer = (double) intersection / (double) combination;
+        if (map1.size() == 0 && map2.size() == 0) {
+            return 65536;
         }
 
-        System.out.println(intersection);
-        System.out.println(combination);
+        for (String key : map1.keySet()) {
+            if (map2.containsKey(key)) {
+                intersection += Math.min(map1.get(key), map2.get(key));
+                combination += Math.max(map1.get(key), map2.get(key));
+                map2.remove(key);
+            } else {
+                combination += map1.get(key);
+            }
+        }
 
-        return (int) (answer * 65536);
+        for (String key : map2.keySet()) {
+            combination += map2.get(key);
+        }
+
+        return (int) Math.floor((intersection / combination) * 65536);
+    }
+
+    private static String addChar(char ch1, char ch2) {
+        return Character.toString(Character.toLowerCase(ch1))
+                + Character.toString(Character.toLowerCase(ch2));
     }
 
     private static boolean isAlphabet(char ch1, char ch2) {
