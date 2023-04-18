c521ff0fe1b31bbb13dd29d800d84898b4c412b2
beombu <beombu13@gmail.com>
2023-04-18 16:33:17 +0900

    Refactor(Programmer) : 타겟 넘버
    Stack 풀이

--- beombu/6주차/타겟_넘버.java
+++ beombu/6주차/타겟_넘버.java
@@ -1,20 +1,41 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class 타겟_넘버 {
-    public static int answer = 0;
 
     public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, target, 0);
+        int answer = 0;
+        int stackNum = 0;
+        int nextNum = 0;
 
-        return answer;
-    }
+        ArrayList<Integer> list;
+        Stack<Integer> stack = new Stack<>();
 
-    public void dfs(int[] numbers, int depth, int target, int sum) {
-        if (depth == numbers.length) {
-            if (target == sum) {
+        stack.push(numbers[0]);
+        stack.push(numbers[0] * -1);
+
+        for (int i = 1; i < numbers.length; i++) {
+            nextNum = numbers[i];
+            list = new ArrayList<>();
+
+            while (!stack.isEmpty()) {
+                stackNum = stack.pop();
+
+                list.add(stackNum + nextNum);
+                list.add(stackNum + (nextNum * -1));
+            }
+
+            for (int j = 0; j < list.size(); j++) {
+                stack.push(list.get(j));
+            }
+        }
+
+        for (Integer sum : stack) {
+            if (sum == target) {
                 answer++;
             }
-        } else {
-            dfs(numbers, depth + 1, target, sum + numbers[depth]);
-            dfs(numbers, depth + 1, target, sum - numbers[depth]);
         }
+
+        return answer;
     }
 }
