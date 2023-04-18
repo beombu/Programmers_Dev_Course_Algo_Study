054fb9cf8b4d46446f52adf3a2e52bdd000a0fe0
beombu <beombu13@gmail.com>
2023-04-18 14:34:41 +0900

    Feat(Programmers) : 타겟 넘버

---
+++ beombu/6주차/타겟_넘버.java
@@ -0,0 +1,20 @@
+public class 타겟_넘버 {
+    public static int answer = 0;
+
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, target, 0);
+
+        return answer;
+    }
+
+    public void dfs(int[] numbers, int depth, int target, int sum) {
+        if (depth == numbers.length) {
+            if (target == sum) {
+                answer++;
+            }
+        } else {
+            dfs(numbers, depth + 1, target, sum + numbers[depth]);
+            dfs(numbers, depth + 1, target, sum - numbers[depth]);
+        }
+    }
+}
