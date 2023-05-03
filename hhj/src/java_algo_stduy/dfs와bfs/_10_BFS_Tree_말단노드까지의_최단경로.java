7e602322c4a0f01e4ff93c4287d86a6d7aa03db6
홍혁준 <jjj8337721@naver.com>
2023-05-03 22:47:44 +0900

    feat: 말단노드까지 최단경로

---
+++ hhj/src/java_algo_stduy/dfs와bfs/_10_BFS_Tree_말단노드까지의_최단경로.java
@@ -0,0 +1,62 @@
+package src.java_algo_stduy.dfs와bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _10_BFS_Tree_말단노드까지의_최단경로 {
+
+    static Node root;
+    static int level = 1;
+
+    public static class Node{
+        int data;
+        Node lt, rt;
+
+        public Node(int data) {
+            this.data = data;
+            this.lt = this.rt = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        init();
+        System.out.println(bfs(root));
+    }
+
+    public static int bfs(Node node){
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for(int i=0; i<size; i++){
+                Node currentNode = queue.poll();
+                if(currentNode.lt == null & currentNode.rt == null){
+                    return level;
+                }
+
+                if(currentNode.lt != null)
+                    queue.offer(currentNode.lt);
+
+                if(currentNode.rt != null)
+                    queue.offer(currentNode.rt);
+            }
+
+            level++;
+        }
+
+        return 0;
+    }
+
+    public static void init(){
+
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+    }
+}
---
+++ hhj/src/java_algo_stduy/dfs와bfs/_9_DFS_Tree_말단노드까지의_최단경로.java
@@ -0,0 +1,42 @@
+package src.java_algo_stduy.dfs와bfs;
+
+/**
+ * 최단 경로이기 때문에 사실 BFS로 푸는 것이 맞음.
+ */
+public class _9_DFS_Tree_말단노드까지의_최단경로 {
+
+    static Node root;
+
+    public static class Node{
+        int data;
+        Node lt, rt;
+
+        public Node(int data) {
+            this.data = data;
+            this.lt = this.rt = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        init();
+        System.out.println(dfs(1, root));
+    }
+
+    public static int dfs(int depth, Node node){
+        if(node.lt == null && node.rt == null){
+            return depth;
+        }
+
+        return Math.min(dfs(depth+1, node.lt), dfs(depth+1, node.rt));
+    }
+
+    public static void init(){
+
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+    }
+}
