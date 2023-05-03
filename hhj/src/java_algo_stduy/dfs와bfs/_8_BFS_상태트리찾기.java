d71c4c3a5781e18fa4eaf8a5df3e7c427b318b23
홍혁준 <jjj8337721@naver.com>
2023-05-03 22:26:47 +0900

    feat: 송아지 찾기

--- hhj/src/java_algo_stduy/dfs와bfs/_8_BFS_상태트리찾기.java
+++ hhj/src/java_algo_stduy/dfs와bfs/_8_BFS_상태트리찾기.java
@@ -1,23 +1,27 @@
-package src;
+package src.java_algo_stduy.dfs와bfs;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Solution {
+/**
+ * 송아지찾기
+ * - 송아지는 움직이지 않고 제자리에 있다.
+ * - 현수는 앞으로 1, 뒤로 1, 앞으로 5 이동 가능
+ * - 최소 몇번의 점프로 현수가 송아지의 위치까지 갈 수 있는 지 구하기
+ */
+public class _8_BFS_상태트리찾기 {
 
     static int answer = 0;
     static boolean[] visit = new boolean[10001];
     static int[] move = new int[]{-1, 1, 5};
+    static Queue<Integer> queue = new LinkedList<>();
 
     public static void main(String[] args) {
-
         System.out.println(bfs(5, 14));
     }
 
     private static int bfs(int start,
                            int end) {
-
-        Queue<Integer> queue = new LinkedList<>();
         queue.offer(start);
         visit[start] = true;
 
