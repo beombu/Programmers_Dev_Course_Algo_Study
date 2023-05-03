3d16ac89388bff142ea8a1aef51162e0f1d40937
홍혁준 <jjj8337721@naver.com>
2023-05-03 19:32:45 +0900

    feat: 피보나치(메모이제이션)

---
+++ hhj/src/고득점kit/dfs와bfs/_4_피보나치_메모이제이션.java
@@ -0,0 +1,24 @@
+package src.고득점kit.dfs와bfs;
+
+public class _4_피보나치_메모이제이션 {
+
+    static int[] answer = new int[11];
+
+    public static void main(String[] args) {
+        dfs(10);
+
+        for(int n : answer){
+            System.out.println(n);
+        }
+    }
+
+    static int dfs(int n){
+        if(answer[n] > 0)
+            return answer[n];
+
+        if(n==1 || n==2)
+            return answer[n] = 1;
+
+        return answer[n] = dfs(n-1) + dfs(n-2);
+    }
+}
