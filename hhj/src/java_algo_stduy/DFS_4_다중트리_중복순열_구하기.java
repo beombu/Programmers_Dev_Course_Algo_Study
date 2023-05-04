108cfe9f6f6ff4ed05e72022dfb4d778e5521322
홍혁준 <jjj8337721@naver.com>
2023-05-04 20:29:47 +0900

    feat : 다중 트리 DFS 중복순열

--- hhj/src/java_algo_stduy/DFS_4_다중트리_중복순열_구하기.java
+++ hhj/src/java_algo_stduy/DFS_4_다중트리_중복순열_구하기.java
@@ -1,4 +1,33 @@
 package src.java_algo_stduy;
 
+/**
+ * N까지 번호가 적힌 구슬
+ * 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열
+ */
 public class DFS_4_다중트리_중복순열_구하기 {
+
+    static int n = 3;
+    static int m = 2;
+
+    public static void main(String[] args) {
+
+        int[] answer = new int[m];
+        dfs(0, answer);
+    }
+
+    public static void dfs(int depth, int[] answer) {
+
+        if(depth == m){
+            for(int a : answer){
+                System.out.print(a + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            answer[depth] = i+1;
+            dfs(depth + 1, answer);
+        }
+    }
 }
