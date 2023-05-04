74bdbae188542e56637aa0b2252fa9eae40ad625
홍혁준 <jjj8337721@naver.com>
2023-05-04 21:05:52 +0900

    feat : 다중 트리 DFS 순열, 조합수

---
+++ hhj/src/java_algo_stduy/dfs_문제/_6_다중트리_순열_구하기.java
@@ -0,0 +1,38 @@
+package src.java_algo_stduy.dfs_문제;
+
+/**
+ * 10이하의 N개 자연수
+ * 이 중 M개를 뽑아 일렬로 나열
+ */
+public class _6_다중트리_순열_구하기 {
+    static int n = 3;   //집합 크기
+    static int m = 2;   //뽑을 갯수
+    static int[] arr = new int[]{3, 6, 9};  //집합
+
+    static int[] answer = new int[m];
+    static boolean[] visit = new boolean[n];
+
+    public static void main(String[] args) {
+        dfs(0);
+    }
+
+    public static void dfs(int depth){
+
+        if(depth == m){
+            for(int i=0; i<m; i++){
+                System.out.print(answer[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            if(!visit[i]) {
+                visit[i] = true;
+                answer[depth] = arr[i];
+                dfs(depth+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
---
+++ hhj/src/java_algo_stduy/dfs_문제/_7_다중트리_조합수_메모이제이션.java
@@ -0,0 +1,25 @@
+package src.java_algo_stduy.dfs_문제;
+
+public class _7_다중트리_조합수_메모이제이션 {
+
+    static int n = 33;
+    static int r = 19;
+
+    static int[][] answer = new int[10001][10001];
+
+    public static void main(String[] args) {
+        System.out.println(dfs(n, r));
+    }
+
+    public static int dfs(int n,
+                          int r){
+        if(answer[n][r] > 0){
+            return answer[n][r];
+        }
+
+        if(n==r || r==0)
+            return answer[n][r] = 1;
+
+        return answer[n][r] = dfs(n-1, r-1) + dfs(n-1, r);
+    }
+}
