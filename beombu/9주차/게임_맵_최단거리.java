0f35be6c70fcc962271859ce036127d11a64c9f7
beombu <beombu13@gmail.com>
2023-05-08 19:54:25 +0900

    Refactor(Programmers) : 게임 맵 최단거리
    - bfs 풀이
    - 최단 거리 문제, depth를 계산 하는 문제

--- beombu/9주차/게임_맵_최단거리.java
+++ beombu/9주차/게임_맵_최단거리.java
@@ -1,41 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class 게임_맵_최단거리 {
-    public static boolean[][] visited;
-    static int answer = -1;
     public static int[] dx = {1, -1, 0, 0};
     public static int[] dy = {0, 0, -1, 1};
 
     public int solution(int[][] maps) {
-        visited = new boolean[maps.length][maps[0].length];
+        int[][] visited = new int[maps.length][maps[0].length];
 
-        dfs(1,0,0,maps);
+        bfs(maps, visited);
+        int answer = visited[maps.length-1][maps[0].length-1];
 
-        return answer;
+        return answer > 0 ? answer : -1;
     }
 
+    private void bfs(int[][] maps, int[][] visited) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+        visited[0][0] = 1;
 
-    private void dfs(int count, int x, int y, int[][] maps) {
-        if (visited[x][y]) {
-            return;
-        }
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int x = current[0];
+            int y = current[1];
 
-        if (x == maps.length - 1 && y == maps[0].length - 1) {
-            if (answer == -1) {
-                answer = count;
-                return;
-            }
-            answer = Math.min(count, answer);
-            return;
-        }
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+                if (nx < 0 || nx > maps.length - 1 || ny < 0 || ny > maps[0].length - 1) {
+                    continue;
+                }
 
-            if (nx >= 0 && nx < visited.length && ny >= 0 && ny < visited[0].length
-                    && !visited[nx][ny] && maps[nx][ny] != 0) {
-                visited[x][y] = true;
-                dfs(count + 1, nx, ny, maps);
-                visited[x][y] = false;
+                if (visited[nx][ny] == 0 && maps[nx][ny] == 1) {
+                    visited[nx][ny] = visited[x][y] + 1;
+                    q.add(new int[]{nx, ny});
+                }
             }
         }
     }
