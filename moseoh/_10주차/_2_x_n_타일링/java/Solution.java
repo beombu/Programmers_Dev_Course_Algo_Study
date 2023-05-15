764735afa17c969f14616287f4616d053f3cb21d
moseoh <azqazq195@gmail.com>
2023-05-15 23:53:42 +0900

    Feat(Programmers): 뒤에 있는 큰 수 찾기

---
+++ moseoh/_10주차/_2_x_n_타일링/java/Solution.java
@@ -0,0 +1,26 @@
+package _10주차._2_x_n_타일링.java;
+
+class Solution {
+    public static void main(String[] args) {
+        int n1 = 4;
+        int answer1 = 5;
+        int result1 = new Solution().solution(n1);
+        PRINT_RESULT(1, result1, answer1);
+    }
+
+    public static void PRINT_RESULT(int index, int result, int answer) {
+        boolean correct = result == answer;
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\n테스트 케이스 ").append(index).append(": ");
+        sb.append(correct ? "정답" : "오답").append("\n");
+        sb.append("\t- 실행 결과: \t").append(result).append("\n");
+        sb.append("\t- 기댓값: \t").append(answer).append("\n");
+        if (correct) System.out.println(sb);
+        else throw new RuntimeException(sb.toString());
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
