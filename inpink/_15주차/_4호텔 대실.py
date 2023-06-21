33681d9628ab700333a557ef1f298ae95406be1c
inpink <108166692+inpink@users.noreply.github.com>
2023-06-21 20:35:27 +0900

    Fe

---
+++ inpink/_15주차/_4호텔 대실.py
@@ -0,0 +1,16 @@
+def time2val(time):
+    hour,min=map(int,time.split(":"))
+    return hour * 60 + min
+
+def solution(book_time):
+    dic = {}
+    for book in book_time:
+        st = time2val(book[0])
+        en = time2val(book[1])
+        for t in range(st,en+10):
+            if t not in dic:
+                dic[t] = 1
+            else:
+                dic[t] += 1
+    
+    return max(dic.values())
